2094e7119e86878c906ffa9c76d95078eaaaf9b9
Redovsky <redaachemlal6@gmail.com>
2023-01-21 22:22:10 +0100

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import Slogo from './Slogo.png';
+
+export {
+  download,
+  logo,
+  Slogo,
+  preview,
+};
